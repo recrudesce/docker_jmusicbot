d9f242048f84a60489a72f3ace0f016a7d78d266
MinecraftCoco <roguish.6888467468@gmail.com>
2020-02-07 22:58:46 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 cd bot;
 if [ ! -f JMusicBot-${VAR}-All.jar ]
 then
-	wget -P https://github.com/jagrosh/MusicBot/releases/download/${VAR}/JMusicBot-${VAR}-All.jar;
+	wget https://github.com/jagrosh/MusicBot/releases/download/${VAR}/JMusicBot-${VAR}-All.jar;
 fi
 java -server -jar JMusicBot-${VAR}-All.jar nogui;
