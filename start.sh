c7475482e62c3b2b782c6efd534fde7c0e2029c0
recrudesce <recrudesce@gmail.com>
2021-12-25 18:59:39 +0000

    Fix nogui

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ if [ ! -f JMusicBot-${VAR}.jar ]
 then
 	wget https://github.com/jagrosh/MusicBot/releases/download/${VAR}/JMusicBot-${VAR}.jar;
 fi
-java -server -jar JMusicBot-${VAR}.jar nogui;
+java -server -jar JMusicBot-${VAR}.jar -Dnogui=true;
